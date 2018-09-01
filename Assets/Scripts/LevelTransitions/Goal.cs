db037ec31d6941664f787e084a354141e88c6237
John Hörnvall <john.minecraft@hotmail.se>
2018-09-01 20:20:39 +0200

    Added level transitions

---
+++ Assets/Scripts/LevelTransitions/Goal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour {
+    public int NextLevel = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Car c = other.GetComponent<Car>();
+        if(c != null)
+        {
+            // Initiate winning routine
+            LevelLoader.levelLoader.LoadLevel(NextLevel);
+        }
+    }
+}
---
+++ Assets/Scripts/LevelTransitions/LevelLoader.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+    public static LevelLoader levelLoader;
+    public SpriteRenderer mask;
+
+    public static float transition = 0f;
+    public static int next_level = 1;
+    public float transition_time = 0.5f;
+
+	// Use this for initialization
+	void Awake () {
+        DontDestroyOnLoad(this.gameObject);
+        DontDestroyOnLoad(mask.gameObject);
+        levelLoader = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Transition
+        transition += Time.deltaTime / (next_level >= 0 ? transition_time : -transition_time);
+
+        if(next_level >= 0 && transition >= 1)
+        {
+            // Go to next level
+            SceneManager.LoadScene(next_level);
+            next_level = -1;
+        }
+
+        // Clamp the transition
+        transition = Mathf.Clamp01(transition);
+
+        // Color the mask
+        mask.color = new Color(0, 0, 0, transition);
+	}
+
+    public void LoadLevel(int next_level)
+    {
+        LevelLoader.next_level = next_level;
+    }
+}
